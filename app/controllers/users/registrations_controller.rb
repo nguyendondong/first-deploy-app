feb0012b44b7514910fe9eaa1a031f7e8576b784
dongnd-3156 <nguyen.don.dong@sun-asterisk.com>
2023-09-20 09:48:11 +0700

    add devise and config rspec

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class Users::RegistrationsController < Devise::RegistrationsController
+  private
+
+  def sign_up_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable, :rememberable, :validatable,
+         :recoverable, :lockable, :confirmable
+
   validates :name, :email, presence: true
 end
