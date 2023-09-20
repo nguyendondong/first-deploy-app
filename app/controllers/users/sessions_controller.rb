e54fcb373cd134ac35466fa249bcb452cf3b917e
dongnd-3156 <nguyen.don.dong@sun-asterisk.com>
2023-09-20 09:48:11 +0700

    add devise and config rspec

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,7 +1,4 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
-  def sign_up_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
 end
